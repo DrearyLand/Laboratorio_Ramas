f60af95d2bc95e2c83e0c9f1866fc86f40da1bf2
Dreary <drearyland22021@gmail.com>
2023-09-15 00:34:16 -0600

    Program Structure

--- Operacion.c
+++ Operacion.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-int suma(int a, int b){
-return a+b;
-}
-int resta(int a, int b){
-return a-b;
-}
->>>>>>> Participante
-=======
->>>>>>> Supervisor
 int main(){
 int num1 = 12;
 int num2 = 3;
@@ -25,8 +13,3 @@ int op3 = multi(num1,num2);
 printf("%i",op3);
 return 0;
 }
-=======
-return 0;
-}
-
->>>>>>> Supervisor
